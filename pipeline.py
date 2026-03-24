603f623ecfc0d747289abc17c6c87a451f6545d0
hitrun polina <phitrun@icloud.com>
2026-03-24 12:02:02 +0300

    start of split clauses

--- pipeline.py
+++ pipeline.py
@@ -8,34 +8,38 @@ from eng_rb_anaphora import resolve_anaphora_en
 from ru_rb_anaphora import resolve_anaphora_ru
 import nltk
 from nltk.tokenize import sent_tokenize
+from tqdm import tqdm
 
 
 if __name__ == "__main__":
     # nltk.download("punkt_tab")
 
-    # winnie = open('pg67098.txt', 'r', encoding='utf-8')
-    # text = winnie.read().replace('\n', ' ')
-    # winnie.close()
+    winnie = open('winnie_ru.txt', 'r', encoding='utf-8')
+    text = winnie.read().replace('\n', ' ')
+    winnie.close()
 
 
-    text = 'Мама зашла в комнату. Бабушка увидела собаку в комнате.'
+    # text = 'Мама зашла в комнату. Бабушка увидела собаку в комнате.'
     lang = 'ru'
     
     sentences = sent_tokenize(text)
     input_path = os.path.abspath("input.conll")
 
     # Разметка TreeTagger
+    print("Preparing input for maltparser...")
     with open(input_path, "w") as f:
-        for sent in sentences:
+        for sent in tqdm(sentences):
             f.write(f"# text = {sent}\n")
             conll_data = prepare.sentence_to_conllx(sent, lang=lang)
             f.write(conll_data)
             f.write("\n")
 
     # Разметка maltparser
+    print("Annotating with maltparser...")
     annotate("input.conll", "output.conll", lang=lang)
 
     # Разрешем анафору по синтаксической разметке
+    print("Resolving anaphora...")
     if lang == 'en':
         resolved_sentences = resolve_anaphora_en("output.conll")
     elif lang == 'ru':
@@ -46,7 +50,7 @@ if __name__ == "__main__":
 
     # with open("output.conll", "r", encoding="utf-8") as f:
     #     conllu_example = f.read()
-
+    print("Building graph...")
     # Строим граф
     if lang == 'en':
         g = build_en_graph(resolved_sentences)
