728387fbfaff8153080a50e61790447c5b1c62f7
hitrun polina <phitrun@icloud.com>
2026-04-02 12:23:01 +0300

    winnie test

--- pipeline.py
+++ pipeline.py
@@ -8,29 +8,41 @@ from eng_rb_anaphora import resolve_anaphora_en
 from ru_rb_anaphora import resolve_anaphora_ru
 import nltk
 from nltk.tokenize import sent_tokenize
+import string
+from tqdm import tqdm
 
 
 if __name__ == "__main__":
     # раскомментировать при первом запуске
     # nltk.download("punkt_tab") 
 
-    text = 'Я дал книгу сестре.'
+    with open("corpus/test/winnie_ru.txt", "r") as f:
+        text = f.read()
     lang = 'ru'
-    
+
+    print('tokenizing and cleaning sentences...')
     sentences = sent_tokenize(text)
+    cleaned_sentences = []
+    for s in sentences:
+        s = s.strip().replace("\n", " ").replace("\t", " ").lower()
+        cleaned_sentences.append("".join(ch for ch in s if ch not in (string.punctuation)))
+    sentences = cleaned_sentences
     input_path = os.path.abspath("input.conll")
 
+    print('preparing data and annotating...')
     # Разметка TreeTagger
     with open(input_path, "w") as f:
-        for sent in sentences:
+        for sent in tqdm(sentences):
             f.write(f"# text = {sent}\n")
             conll_data = prepare.sentence_to_conllx(sent, lang=lang)
             f.write(conll_data)
             f.write("\n")
 
+    print('annotating with maltparser...')
     # Разметка maltparser
     annotate("input.conll", "output.conll", lang=lang)
 
+    print('anaphora resolution')
     # Разрешем анафору по синтаксической разметке
     if lang == 'en':
         resolved_sentences = resolve_anaphora_en("output.conll")
@@ -39,7 +51,7 @@ if __name__ == "__main__":
         print("\nResolved sentences:")
         for sent in resolved_sentences:
             print(sent.text)
-
+    print('building graph...')
     # Строим граф
     if lang == 'en':
         g = build_en_graph(resolved_sentences)
@@ -50,4 +62,7 @@ if __name__ == "__main__":
     for e in g.edges:
         print(f"{e.agent_1} --[{e.meaning}]--> {e.agent_2}")
 
-    visualize_graph_interactive(g, output="graph.html")
+    g.save_to_csv("winnie_ru_graph.csv")
+    print("Graph saved to winnie_ru_graph.csv")
+
+    visualize_graph_interactive(g, output="graph_ru.html")
